22f61b39d738b81e1542c06fbb8d69dbbfd79a6a
Denys Grybov <denys@grybov.com>
2020-10-24 17:07:18 -0500

    Update api service

--- template/src/services/api.ts
+++ template/src/services/api.ts
@@ -1,8 +1,8 @@
-import { stringify } from 'query-string';
+import { StringifiableRecord, stringify } from 'query-string';
 
 const fetchProps = {};
 
-export const GET = <T, P = {}>(url: string, params?: P) => {
+export const GET = <T>(url: string, params?: StringifiableRecord) => {
   const uri = params ? `?${stringify(params)}` : '';
 
   return fetch(url + uri, {
